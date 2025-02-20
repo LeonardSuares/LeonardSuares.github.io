f9a19e4e6c53e792bb41a3f1b1e71a786ffcf2ed
LeonardSuares <leonard.suares@gmail.com>
2025-02-20 15:26:34 -0500

    Removed tailwind.config.js since I am using app router

---
+++ my-portfolio/src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold">About Me</h1>
+      <p className="mt-4 text-lg text-gray-400">I am a developer passionate about building interactive applications.</p>
+    </div>
+  );
+}
