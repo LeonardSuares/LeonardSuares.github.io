988b6c00e0cc10f35b5175342a1f13b2bcbd7b07
LeonardSuares <leonard.suares@gmail.com>
2025-02-20 14:20:46 -0500

    re-arranged the page files

--- my-portfolio/pages/Index.js
+++ my-portfolio/pages/Index.js
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center">
       <nav className="w-full p-4 flex justify-center space-x-6 bg-gray-800 fixed top-0">
-        <Link href="/" className="hover:text-blue-400">Home</Link>
         <Link href="/about" className="hover:text-blue-400">About</Link>
-        <Link href="/projects" className="hover:text-blue-400">Projects</Link>
         <Link href="/contact" className="hover:text-blue-400">Contact</Link>
+        <Link href="/" className="hover:text-blue-400">Home</Link>
+        <Link href="/projects" className="hover:text-blue-400">Projects</Link>
       </nav>
       
       <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }} className="text-center mt-20">
